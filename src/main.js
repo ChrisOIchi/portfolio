bd93c2d910a50e210e4e826530811411bcf8c74f
ChristopherObed <obedosunae@gmail.com>
2024-01-29 22:13:25 -0800

    Refactor tooltip styling and add copy-to-clipboard functionality

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var copyEmailBtn = document.getElementById("copyEmailBtn");
-  
-    copyEmailBtn.addEventListener("click", function() {
-      // Lógica para copiar el correo electrónico al portapapeles
+  var copyEmailBtn = document.getElementById("copyEmailBtn");
+  var copyTooltip = document.getElementById("copyTooltip");
+
+  copyEmailBtn.addEventListener("click", function(event) {
       var emailText = copyEmailBtn.innerText;
-      var tempTextArea = document.createElement("textarea");
-      tempTextArea.value = emailText;
-      document.body.appendChild(tempTextArea);
-      tempTextArea.select();
-      document.execCommand("copy");
-      document.body.removeChild(tempTextArea);
-      alert("Correo copiado al portapapeles: " + emailText);
-    });
+
+      // Utilizar el API de Clipboard para copiar al portapapeles
+      navigator.clipboard.writeText(emailText).then(function() {
+          // Posicionar el tooltip cerca del puntero
+          copyTooltip.style.top = (event.clientY - 30) + "px"; // Ajusta la posición vertical según tu preferencia
+          copyTooltip.style.left = event.clientX + "px";
+
+          // Mostrar el tooltip
+          copyTooltip.classList.add("active");
+
+          // Ocultar el tooltip después de 2 segundos
+          setTimeout(function() {
+              copyTooltip.classList.remove("active");
+          }, 2500);
+      }).catch(function(err) {
+          console.error('Error al copiar al portapapeles: ', err);
+      });
   });
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuToggle = document.getElementById('mobileMenuToggle');
-    const navbarMenu = document.querySelector('.navbar-menu');
-
-    mobileMenuToggle.addEventListener('click', function () {
-        navbarMenu.classList.toggle('active');
-    });
 });
+
+
