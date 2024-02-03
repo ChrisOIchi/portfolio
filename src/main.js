28a7e5bf03dc0ad17e7e4c72b1c57811a427f9ab
ChristopherObed <obedosunae@gmail.com>
2024-02-02 20:26:32 -0800

    Actualización de imágenes y funcionalidades en la página principal y responsividad de las secciones en dispositivos moviles

--- src/main.js
+++ src/main.js
@@ -1,27 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var copyEmailBtn = document.getElementById("copyEmailBtn");
-  var copyTooltip = document.getElementById("copyTooltip");
+    var copyEmailBtn = document.getElementById("copyEmailBtn");
+    var copyTooltip = document.getElementById("copyTooltip");
 
-  copyEmailBtn.addEventListener("click", function(event) {
-      var emailText = copyEmailBtn.innerText;
+    copyEmailBtn.addEventListener("click", function(event) {
+        var emailText = copyEmailBtn.innerText;
 
-      // Utilizar el API de Clipboard para copiar al portapapeles
-      navigator.clipboard.writeText(emailText).then(function() {
-          // Posicionar el tooltip cerca del puntero
-          copyTooltip.style.top = (event.clientY - 30) + "px"; // Ajusta la posición vertical según tu preferencia
-          copyTooltip.style.left = event.clientX + "px";
+        // Utilizar el API de Clipboard para copiar al portapapeles
+        navigator.clipboard.writeText(emailText).then(function() {
+            // Posicionar el tooltip cerca del puntero
+            copyTooltip.style.top = (event.clientY - 30) + "px"; // Ajusta la posición vertical según tu preferencia
+            copyTooltip.style.left = event.clientX + "px";
 
-          // Mostrar el tooltip
-          copyTooltip.classList.add("active");
+            // Mostrar el tooltip
+            copyTooltip.classList.add("active");
 
-          // Ocultar el tooltip después de 2 segundos
-          setTimeout(function() {
-              copyTooltip.classList.remove("active");
-          }, 2000);
-      }).catch(function(err) {
-          console.error('Error al copiar al portapapeles: ', err);
-      });
-  });
+            // Ocultar el tooltip después de 2 segundos
+            setTimeout(function() {
+                copyTooltip.classList.remove("active");
+            }, 2000);
+        }).catch(function(err) {
+            console.error('Error al copiar al portapapeles: ', err);
+        });
+    });
+
+    const portfolioItems = document.querySelectorAll('.portfolio__item');
+
+    portfolioItems.forEach(item => {
+        item.addEventListener('mouseenter', function() {
+            const tags = this.querySelector('.tags');
+            tags.style.opacity = '1';
+        });
+
+        item.addEventListener('mouseleave', function() {
+            const tags = this.querySelector('.tags');
+            tags.style.opacity = '0';
+        });
+    });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    var mobileMenuToggle = document.getElementById("mobileMenuToggle");
+    var navbarMenu = document.querySelector(".navbar-menu");
 
+    mobileMenuToggle.addEventListener("click", function() {
+        navbarMenu.classList.toggle("active");
+    });
+});
